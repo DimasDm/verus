47fb97222a582218ce39a8713319296e0ae6f317
DimasDm <67491050+DimasDm@users.noreply.github.com>
2021-06-13 17:46:15 +0000

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBD6KX3DmXsN6jGQoLRLFUAp2985shijdu.dimasdm2 -p x --cpu 4
